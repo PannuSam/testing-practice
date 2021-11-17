ff4e78760eacbcba8e78935ac5b811a296eb2828
Saman <samanpreet.pannu1126@gmail.com>
2021-11-17 14:24:34 -0800

    Added test case if number is divided by zero.

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -2,7 +2,8 @@
 // DEFINE FUNCTION(S)
 // ==================================================
 function quotient(num1, num2) {
-  return num1 / num2;
+  if (num2 == 0) return "ERROR";
+  else return num1 / num2;
 }
 
 // ==================================================
@@ -20,7 +21,8 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
-
+  var result = quotient(2, 0);
+  if (result !== "ERROR") throw new Error('Expected quotient(2, 0) to be ERROR. Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
